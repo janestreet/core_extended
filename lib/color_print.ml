d9f996274c2b257d41a85f8dd546850ce98bf42e
Jeremie Dimino <jdimino@janestreet.com>
2013-05-14 10:06:20 +0100

    109.23.00

--- lib/color_print.ml
+++ lib/color_print.ml
@@ -90,7 +90,8 @@ let color str ~color      = wrap str ~code:(color_code ~color)
    This is probably not necessary, but it helps ensure that the code that turns
    off the special formatting actually does get output *)
 let wrap_print ~code fmt =
-  printf "%s" code; Printf.kfprintf (fun oc -> fprintf oc "%s%!" normal_code) stdout fmt
+  Printf.kfprintf (fun oc -> fprintf oc "%s%!" normal_code) stdout
+    ("%s" ^^ fmt) code
 
 let boldprintf      fmt = wrap_print ~code:bold_code      fmt
 let underlineprintf fmt = wrap_print ~code:underline_code fmt
@@ -105,100 +106,3 @@ let cyanprintf      fmt = wrap_print ~code:cyan_code      fmt
 let grayprintf ~brightness fmt = wrap_print ~code:(gray_code ~brightness) fmt
 let rgbprintf ~r ~g ~b     fmt = wrap_print ~code:(rgb_code ~r ~g ~b)     fmt
 let colorprintf ~color     fmt = wrap_print ~code:(color_code ~color)     fmt
-
-
-(* module Style = struct
- *   type 'a fmt = ('a,out_channel,unit) format
- *
- *   module type T = sig
- *     val red     : 'a fmt -> 'a fmt
- *     val yellow  : 'a fmt -> 'a fmt
- *     val green   : 'a fmt -> 'a fmt
- *     val blue    : 'a fmt -> 'a fmt
- *     val inverse : 'a fmt -> 'a fmt
- *   end
- *
- *   type t = (module T)
- *
- *   let ansi =
- *     let module M = struct
- *       let style code fmt = "\027[" ^^ code ^^ "m" ^^ fmt ^^ "\027[0m"
- *
- *       let red     fmt = style "31" fmt
- *       let yellow  fmt = style "33" fmt
- *       let green   fmt = style "32" fmt
- *       let blue    fmt = style "34" fmt
- *       let inverse fmt = style "7"  fmt
- *     end
- *     in
- *     (module M : T)
- *
- *   let no_formatting =
- *     let module M = struct
- *       let red      = Fn.id
- *       let yellow   = Fn.id
- *       let green    = Fn.id
- *       let blue     = Fn.id
- *       let inverse  = Fn.id
- *     end
- *     in
- *     (module M : T)
- *
- *   let ok_or_not =
- *     let module M = struct
- *       let ok     fmt = "ok:     " ^^ fmt
- *       let not_ok fmt = "not ok: " ^^ fmt
- *
- *       let red     = not_ok
- *       let yellow  = not_ok
- *       let green   = ok
- *       let blue    = Fn.id
- *       let inverse = Fn.id
- *     end
- *     in
- *     (module M : T)
- * end
- *
- * module type T = sig
- *   val println        : string -> unit
- *   val redprintln     : string -> unit
- *   val yellowprintln  : string -> unit
- *   val greenprintln   : string -> unit
- *   val blueprintln    : string -> unit
- *   val inverseprintln : string -> unit
- *   val printfln        : 'a Style.fmt -> 'a
- *   val redprintfln     : 'a Style.fmt -> 'a
- *   val yellowprintfln  : 'a Style.fmt -> 'a
- *   val greenprintfln   : 'a Style.fmt -> 'a
- *   val blueprintfln    : 'a Style.fmt -> 'a
- *   val inverseprintfln : 'a Style.fmt -> 'a
- * end
- *
- * type t = (module T)
- *
- * let create ~style ~oc =
- *   ignore style;
- *   let module M = struct
- *     include (val style : Style.T)
- *
- *     let nl_flsh fmt = fmt ^^ "\n%!"
- *     let nl_flsh_fmt = nl_flsh "%s"
- *
- *     let println        = fprintf oc nl_flsh_fmt
- *     let redprintln     = fprintf oc (red     nl_flsh_fmt)
- *     let yellowprintln  = fprintf oc (yellow  nl_flsh_fmt)
- *     let greenprintln   = fprintf oc (green   nl_flsh_fmt)
- *     let blueprintln    = fprintf oc (blue    nl_flsh_fmt)
- *     let inverseprintln = fprintf oc (inverse nl_flsh_fmt)
- *
- *     let printfln        fmt = fprintf oc (nl_flsh fmt)
- *     let redprintfln     fmt = fprintf oc (nl_flsh (red fmt))
- *     let yellowprintfln  fmt = fprintf oc (nl_flsh (yellow fmt))
- *     let greenprintfln   fmt = fprintf oc (nl_flsh (green fmt))
- *     let blueprintfln    fmt = fprintf oc (nl_flsh (blue fmt))
- *     let inverseprintfln fmt = fprintf oc (nl_flsh (inverse fmt))
- *   end
- *   in
- *   (module M : T)
- *
- * module S = (val create ~style:Style.ansi ~oc:stdout : T) *)
--- lib/color_print.mli
+++ lib/color_print.mli
@@ -62,36 +62,3 @@ val cyanprintf     : ('a, out_channel, unit) format -> 'a
 val grayprintf : brightness:float              -> ('a, out_channel, unit) format -> 'a
 val rgbprintf  : r:float -> g:float -> b:float -> ('a, out_channel, unit) format -> 'a
 val colorprintf: color:color                   -> ('a, out_channel, unit) format -> 'a
-
-(* module Style : sig
- *   type t
- *   val ansi : t
- *   val no_formatting : t
- *   (\* no ansi formatting, but prints "not ok:" for yellow or red and "ok:" for green *\)
- *   val ok_or_not : t
- * end
- *
- * module type T = sig
- *   val println        : string -> unit
- *   val redprintln     : string -> unit
- *   val yellowprintln  : string -> unit
- *   val greenprintln   : string -> unit
- *   val blueprintln    : string -> unit
- *   val inverseprintln : string -> unit
- *   val printfln        : ('a, out_channel, unit) format -> 'a
- *   val redprintfln     : ('a, out_channel, unit) format -> 'a
- *   val yellowprintfln  : ('a, out_channel, unit) format -> 'a
- *   val greenprintfln   : ('a, out_channel, unit) format -> 'a
- *   val blueprintfln    : ('a, out_channel, unit) format -> 'a
- *   val inverseprintfln : ('a, out_channel, unit) format -> 'a
- * end
- *
- * type t = (module T)
- *
- * val create :
- *   style:Style.t
- *   -> oc:Out_channel.t
- *   -> t
- *
- * (\* ansi on standard out *\)
- * module S : T *)
