04a50083538d4ef3b9646be5d377d5ee544a4a33
Thomas Refis <trefis@vdc-qws-u12116a.delacy.com>
2017-09-13 13:28:07 +0100

    v0.9.115.15+72

--- src/random_selection.ml
+++ src/random_selection.ml
@@ -6,11 +6,12 @@
 open Core
 
 type 'a t = {
-  rand : int -> int;
-  desired_sample_size : int;
-  selection : 'a option array;
-  num_seen : int ref;
+  rand                : (int -> int)      ;
+  desired_sample_size : int               ;
+  selection           : 'a   option array ;
+  num_seen            : int  ref          ;
 }
+[@@deriving fields]
 
 let desired_sample_size t = t.desired_sample_size
 
@@ -48,3 +49,47 @@ let select ?random_state ~next desired_sample_size =
   loop ();
   to_list t
 
+let clear t =
+  Fields.iter
+    ~rand:                ignore
+    ~desired_sample_size: ignore
+    ~selection:           (fun _ -> Array.iteri t.selection ~f:(fun i _ -> t.selection.(i) <- None))
+    ~num_seen:            (fun _ -> t.num_seen := 0)
+
+let num_seen t = !(t.num_seen)
+
+let%expect_test _ =
+  let size = 3 in
+  let t = create ~random_state:(Random.State.make [| 17 |]) size in
+  for i = 0 to 9 do
+    maybe_add t i;
+    printf !"num_seem=%d %{sexp#mach: int list}\n" (num_seen t) (to_list t);
+  done;
+  [%expect {|
+    num_seem=1 (0)
+    num_seem=2 (0 1)
+    num_seem=3 (0 1 2)
+    num_seem=4 (0 1 3)
+    num_seem=5 (4 1 3)
+    num_seem=6 (4 1 5)
+    num_seem=7 (4 1 6)
+    num_seem=8 (4 1 6)
+    num_seem=9 (4 1 6)
+    num_seem=10 (9 1 6) |}];
+  clear t;
+  for i = 0 to 9 do
+    maybe_add t i;
+    printf !"num_seem=%d %{sexp#mach: int list}\n" (num_seen t) (to_list t);
+  done;
+  [%expect {|
+    num_seem=1 (0)
+    num_seem=2 (0 1)
+    num_seem=3 (0 1 2)
+    num_seem=4 (3 1 2)
+    num_seem=5 (3 1 4)
+    num_seem=6 (3 1 4)
+    num_seem=7 (6 1 4)
+    num_seem=8 (6 7 4)
+    num_seem=9 (6 7 4)
+    num_seem=10 (6 7 4) |}]
+;;
--- src/random_selection.mli
+++ src/random_selection.mli
@@ -5,7 +5,7 @@ open! Core
 type 'a t (** a random sample of ['a] values *)
 
 (** [create ~random_state desired_sample_size] creates an empty sample of ['a] values.
-    The sample will grow no larger than [desired_sample_size] when presented with more 
+    The sample will grow no larger than [desired_sample_size] when presented with more
     values by calling [add]. *)
 val create : ?random_state:Random.State.t -> int -> 'a t
 
@@ -25,4 +25,11 @@ val to_list : 'a t -> 'a list
 (** randomly select a subset of size [sample_size] from a stream of unknown length.
     Each possible subset is chosen with equal probability. *)
 val select : ?random_state:Random.State.t -> next:(unit -> 'a option) -> int -> 'a list
+
+(** number of things [maybe_add]ed so far, no matter they're actually added or ignored *)
+val num_seen : _ t -> int
+
+(** [clear t] clears contents of [t] and resets [num_seen]. It doesn not reset random
+    state *)
+val clear : _ t -> unit
 
